11d05c80e0da350a41642fcace24f702065f3eb3
mgrgic1 <marko.grgic344@gmail.com>
2020-10-29 18:58:48 -0400

    Added user income

--- models/index.js
+++ models/index.js
@@ -1,11 +1,15 @@
 const Users = require('./Users');
 const UserGoals = require('./UserGoals');
 const UserExpenses = require('./UserExpenses');
+const UserIncome = require('./UserIncome');
 
 UserGoals.belongsTo(Users);
 UserExpenses.belongsTo(Users);
+UserIncome.belongsTo(Users);
+
 Users.hasMany(UserExpenses);
 Users.hasMany(UserGoals);
+Users.hasMany(UserIncome);
 
 
 
@@ -13,4 +17,5 @@ module.exports = {
     Users,
     UserGoals,
     UserExpenses,
+    UserIncome,
 };
