a07cacb9636b4077eaffb8da249037f1e8209b89
mgrgic1 <marko.grgic344@gmail.com>
2020-11-02 17:03:03 -0500

    Updated user income routes

--- routes/userIncomeRoutes.js
+++ routes/userIncomeRoutes.js
@@ -20,34 +20,74 @@ router.get('/incomeById/:incomeId', async (req,res,next) => {
     }
 })
 
-//GET all income for a particular user
-router.get('/:userId', async (req,res,next) => {
-    const {userId} = req.params;
+//GET all the current user's income
+router.get('/currentUser', async (req,res) => {
+    const income = await UserIncome.findAll(
+        { where: {userId: req.user.dataValues.id}, 
+          order: [
+            ['incomeId','DESC'],
+          ] 
+        }
+    )
+    res.send(income);
+});
+
+
+//GET all income for the current month
+router.get('/currentUser/currentMonth', async (req,res, next) => {
+    let d = new Date();
+    let currentMonth = d.getMonth() + 1;
+    let userId = req.user.dataValues.id;
     
     UserIncome.findAll({
         where: {
-            user_id: [userId]
+            user_id: userId,
+            income_month: [currentMonth]
         },
-        //highest MONTHLY income go first
         order: [
             ['income_monthly','DESC'],
         ]
     })
     .then(
-        incomeRes => res.send(incomeRes)
+        monthIncome => res.send(monthIncome)
     )
-    .catch(next)  
-})
+    .catch(next)
+});
+
+
+//GET ALL income by month and year
+//ex: /currentUser/getIncomeByMY/10/2020 would return all income for october 2020
+router.get('/currentUser/getIncomeByMY/:month/:year', async (req,res, next) => {
+    let month = req.params.month;
+    let year = req.params.year;
+    let userId = req.user.dataValues.id;
+
+    UserIncome.findAll({
+        where: {
+            user_id: userId,
+            income_month: [month],
+            income_year: [year]
+        },
+        order: [
+            ['income_monthly','DESC'],
+        ]
+    })
+    .then(
+        MYIncome => res.send(MYIncome)
+    )
+    .catch(next)
+});
+
 
 //POST to create a new user income (based on USER_ID)
 router.post('/add', (req,res) => {
 
-    let {user_id, income_monthly, income_type, description, real_amount, real_frequency} = req.body;
+    let {user_id, income_monthly, income_type, description, income_month, income_year, real_amount, real_frequency} = req.body;
 
 
     //insert into table
     UserIncome.create({
-        user_id, income_monthly, income_type, description, real_amount, real_frequency
+        user_id, income_monthly, income_type, description, income_month, income_year, real_amount, real_frequency
     }) 
     .then(userIncome => res.send(userIncome))
     .catch(err => console.log(err));
